c8d148d7e98818172c74d1b5447ade9817b181f1
peterchuks1994 <85123514+peterchuks1994@users.noreply.github.com>
2026-08-28 09:07:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+output: 'server',
+adapter: cloudflare()
 });
